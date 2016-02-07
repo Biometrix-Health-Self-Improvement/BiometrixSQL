8bcbc8721e4d5de3130fee7ca122f29467e80cec
Troy Riblett <troy.riblett@oit.edu>
2016-02-06 22:04:58 -0800

    Added isGoogle Field
    Added a bit field isGoogle for whether the user is a google user or not.
    If the user is not, no password is required.

--- Drop And Add LoginTable.sql
+++ Drop And Add LoginTable.sql
@@ -15,9 +15,9 @@ GO
 CREATE TABLE [dbo].[LoginTable](
 	[UserID] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	[Username] [nvarchar](40) NOT NULL UNIQUE,
-	[Password] [nvarchar](255) NOT NULL,
+	[Password] [nvarchar](255) NULL,
 	[Email] [nvarchar](40) NOT NULL UNIQUE,
+	[IsGoogle] [bit] NOT NULL DEFAULT 0,
+	CONSTRAINT chk_google CHECK ([Password] is not null OR [IsGoogle] = 1)
 )
-GO
-
-
+GO 

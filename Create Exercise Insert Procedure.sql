47cd40e4c731026abd511fc82a395c842026845d
Troy Riblett <troy.riblett@oit.edu>
2016-02-28 13:02:22 -0800

    Updated Insert Procedure to work better with Nulls
    Some issues with passing nulls from the webserver. Now I essentially
    check for them manually.

--- Create Exercise Insert Procedure.sql
+++ Create Exercise Insert Procedure.sql
@@ -6,14 +6,61 @@ DROP PROCEDURE ExerciseInsert
 END
 GO
 
-CREATE PROCEDURE ExerciseInsert @UserID INT, @Title VARCHAR(255), @Type VARCHAR(140), @MINUTES TINYINT,
-@Reps TINYINT, @Laps TINYINT, @Weight SMALLINT, @Inty TINYINT, @Notes VARCHAR(255), @DateEx DATE, @TimeEx Varchar(50)
+CREATE PROCEDURE ExerciseInsert @UserID VARCHAR(50), @Title VARCHAR(255), @Type VARCHAR(140), @Minutes VARCHAR(50),
+@Reps VARCHAR(50), @Laps VARCHAR(50), @Weight VARCHAR(50), @Inty VARCHAR(50), @Notes VARCHAR(255), @DateEx DATE, @TimeEx Varchar(50)
 AS
+
+DECLARE @UserID2 INT;
+DECLARE @Minutes2 TINYINT;
+DECLARE @Reps2 TINYINT;
+DECLARE @Laps2 TINYINT;
+DECLARE @Weight2 SMALLINT;
+DECLARE @Inty2 TINYINT;
+
+
+Set @UserID2 = TRY_CONVERT(INT, @UserID);
+Set @Minutes2 = TRY_CONVERT(TINYINT, @Type);
+Set @Reps2 = TRY_CONVERT(TINYINT, @Reps);
+Set @Laps2 = TRY_CONVERT(TINYINT, @Laps);
+Set @Weight2 = TRY_CONVERT(SMALLINT, @Weight);
+Set @Inty2 = TRY_CONVERT(TINYINT, @Inty);
+
+IF @UserID2 = 0
+BEGIN
+	Set @UserID2 = NULL;
+END
+
+IF @Minutes2 = 0
+BEGIN
+	Set @Minutes2 = NULL;
+END
+
+IF @Reps2 = 0
+BEGIN
+	Set @Reps2 = NULL;
+END
+
+IF @Laps2 = 0
+BEGIN
+	Set @Laps2 = NULL;
+END
+
+IF @Weight2 = 0
+BEGIN
+	Set @Weight2 = NULL;
+END
+
+IF @Inty2 = 0
+BEGIN
+	Set @Inty2 = NULL;
+END
+
+
 
 Insert Into dbo.Exercise
 ([UserID], [Title], [Type], [Minutes], [Reps], [Laps], [Weight], [Inty], [Notes], [DateEx], [TimeEx])
 Values
-(@UserID, @Title, @Type, @Minutes, @Reps, @Laps, @Weight, @Inty, @Notes, @DateEx, @TimeEx)
+(@UserID2, @Title, @Type, @Minutes2, @Reps2, @Laps2, @Weight2, @Inty2, @Notes, @DateEx, @TimeEx)
 
 GO
 
--- Create Sleep Insert Procedure.sql
+++ Create Sleep Insert Procedure.sql
@@ -10,10 +10,34 @@ CREATE PROCEDURE SleepInsert @UserID INT, @LocalSleepID INT, @Date DATE,
 @Time TIME, @Duration TIME, @Quality INT, @Notes VARCHAR(300), @Health VARCHAR(20)
 AS
 
+DECLARE @UserID2 INT;
+DECLARE @LocalSleepID2 INT;
+DECLARE @Quality2 INT;
+
+
+Set @UserID2 = TRY_CONVERT(INT, @UserID);
+Set @LocalSleepID2 = TRY_CONVERT(INT, @LocalSleepID);
+Set @Quality2 = TRY_CONVERT(INT, @Quality);
+
+IF @UserID2 = 0
+BEGIN
+	Set @UserID2 = NULL;
+END
+
+IF @LocalSleepID2 = 0
+BEGIN
+	Set @LocalSleepID2 = NULL;
+END
+
+IF @Quality2 = 0
+BEGIN
+	Set @Quality2 = NULL;
+END
+
 Insert Into dbo.Sleep
 ([UserID], [LocalSleepID], [Date], [Time], [Duration], [Quality], [Notes], [Health])
 Values
-(@UserID, @LocalSleepID, @Date, @Time, @Duration, @Quality, @Notes, @Health)
+(@UserID2, @LocalSleepID2, @Date, @Time, @Duration, @Quality2, @Notes, @Health)
 
 GO
 
